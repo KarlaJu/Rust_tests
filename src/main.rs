c771f5880b30431c5ef22901d9c0d8b020abbc7d
Karla <karla@makingdevs.com>
2018-09-19 13:25:36 -0500

    Assert equal and not equal

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 #[cfg(test)]
 mod firs_test {
-#[test]
-#[should_panic]    
+  #[test]
+  #[should_panic]    
     fn test(){
-    assert!(1==1);
-    panic!("Oh nooo!");
+      assert!(1==1);
+      panic!("Oh nooo!");
+    }
+
+
+  #[test]
+    fn equal_test(){
+      assert_eq!(2, 1 + 1);
+      assert_ne!(2, 1 + 2);
     }
 }

33341d3b883d9cf73b318622a12554b70235d413
Karla <karla@makingdevs.com>
2018-09-19 13:39:27 -0500

    Adding rectangle struct and test for size

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+struct Rectangle{
+  width: u8,
+  height: u8
+}
+
+impl Rectangle{
+  fn is_square(&self) -> bool {
+    self.width == self.height
+  }
+}
+
 fn get_two() -> i16 {
   2
 }
+
 #[cfg(test)]
 mod firs_test {
   #[test]
@@ -23,5 +35,14 @@ mod firs_test {
       assert_eq!(number, 1 + 1);
       assert_ne!(number, 1 + 2);
     }
-  
+
+  #[test]
+  #[should_panic]
+    fn test_struct_rectangle(){
+      let size = super::Rectangle {
+        width: 50,
+        height: 25
+      };
+      assert!(size.is_square());
+    }  
 }
